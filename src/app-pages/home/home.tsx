fad7c78105b19c6639b486150a9373f1923e5135
ye1sultan <niyaztaye@gmail.com>
2025-05-28 19:26:50 +0500

    feat: move people section to features

--- src/app-pages/home/home.tsx
+++ src/app-pages/home/home.tsx
@@ -1,9 +1,12 @@
-import { Button } from '@/src/components/ui/button';
+import People from '@/src/features/people/people';
+
+import { Breadcrumbs } from '@/src/widgets/breadcrumbs/breadcrumbs';
 
 export default function Home() {
   return (
-    <>
-      <Button className="w-fit">Загрузить</Button>
-    </>
+    <div className="mx-auto flex w-full max-w-[1200px] flex-col gap-4 p-4">
+      <Breadcrumbs items={[{ label: 'Catalogue', href: '/' }]} />
+      <People />
+    </div>
   );
 }
