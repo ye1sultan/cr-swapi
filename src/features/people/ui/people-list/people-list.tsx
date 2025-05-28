af831ddd95e291bd4821497b1994bf298e2b1fe4
ye1sultan <niyaztaye@gmail.com>
2025-05-28 19:39:25 +0500

    feat: handle no characters

--- src/features/people/ui/people-list/people-list.tsx
+++ src/features/people/ui/people-list/people-list.tsx
@@ -3,6 +3,8 @@ import { IPerson } from '@/src/types/person';
 import { PeopleCard } from './ui/people-card/people-card';
 
 export function PeopleList({ people }: { people: IPerson[] }) {
+  if (people.length === 0) return <div>No characters found :(</div>;
+
   return (
     <>
       {people.map((person: IPerson, index: number) => (
