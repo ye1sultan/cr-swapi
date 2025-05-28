5a65a406d2213e7a88e3d68b6ef2ad19ab920685
ye1sultan <niyaztaye@gmail.com>
2025-05-28 16:23:15 +0500

    feat: copy all shadcn typography

---
+++ src/components/ui/typography/lead.tsx
@@ -0,0 +1,3 @@
+export function TypographyLead({ children }: { children: React.ReactNode }) {
+  return <p className="text-muted-foreground text-xl">{children}</p>;
+}
---
+++ src/components/ui/typography/muted.tsx
@@ -0,0 +1,3 @@
+export function TypographyMuted({ children }: { children: React.ReactNode }) {
+  return <p className="text-muted-foreground text-sm">{children}</p>;
+}
