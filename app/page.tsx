f4653de3077b4268632c9202a713b2abe2a52b33
ye1sultan <niyaztaye@gmail.com>
2025-05-28 19:31:46 +0500

    feat: wrap useSearchParams with Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import { Suspense } from 'react';
+
+import { Loader2Icon } from 'lucide-react';
+
 import Home from '@/src/app-pages/home/home';
 
 export default function Page() {
-  return <Home />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex h-full w-full items-center justify-center">
+          <Loader2Icon className="animate-spin" />
+        </div>
+      }
+    >
+      <Home />
+    </Suspense>
+  );
 }
